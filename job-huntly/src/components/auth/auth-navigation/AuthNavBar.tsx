67a7007bdbd4e94ca5644c2b3f906a1161f27df8
Joel Hernandez <you@example.com>
2023-12-11 08:44:27 -0500

    add useMatchMedia hook

--- job-huntly/src/components/auth/auth-navigation/AuthNavBar.tsx
+++ job-huntly/src/components/auth/auth-navigation/AuthNavBar.tsx
@@ -1,24 +1,26 @@
 'use client';
 
 import useAuthNav from "@/hooks/useAuthNav";
+import useMatchMedia from "@/hooks/useMatchMedia";
 import Button from "@/components/Button/Button";
 import CompanyLogo from "@/components/companyLogo/CompanyLogo";
 import auth from '../auth.module.css';
- 
+
 const AuthNavBar = () => {
   const {currentPath,navigateToLogin,navigateToSignUp} = useAuthNav();
+  const {hasCurrentViewportMatch} = useMatchMedia({ media: '(min-width: 1000px)' });
   
   return (
     <nav className={auth.auth_nav_wrapper}>
     <CompanyLogo theme="light"/>
      
      {currentPath === 'login' ? (
-       <Button type="primary" size="small" handleClick={navigateToSignUp}>
+       <Button type="primary" size={!hasCurrentViewportMatch?'small' : 'large'}  handleClick={navigateToSignUp}>
          Sign Up
       </Button>
      ):
      (
-      <Button type="primary" size="small" handleClick={navigateToLogin}>
+      <Button type="primary"  size={!hasCurrentViewportMatch?'small' : 'large'} handleClick={navigateToLogin}>
         Login
      </Button>
      )
